5769657d5b782e680ee6eeb2479a65f251207fe2
cricri972 <olc972@gmail.com>
2017-04-11 15:00:34 -0400

    update avec remplissage formulaire

--- app/Controller/ShopController.php
+++ app/Controller/ShopController.php
@@ -189,22 +189,144 @@ class ShopController extends Controller
 
 		echo '<br>';
 		$info = ['shop' => $viewShop]; 
-
 		//var_dump($info);
 
 		$this->show('shop/viewShop', $info);
-
 	}
+/* ------------------- UPDATE SHOP -------------------------------------------------*/
 
 	public function updateShop($id)
 	{
-		$updateShopModel = new ShopModel();
-		$updateShop = $updateShopModel -> update($id);
+		$errors = [];
+		$post = [];
+		//var_dump($_POST);
+		$maxSize = (1024 * 1000) * 2; // Taille maximum du fichier
+		//$uploadDir = 'HHH/uploads/'; // Répertoire d'upload
+		$mimeTypeAvailable = ['image/jpg', 'image/jpeg', 'image/pjpeg', 'image/png', 'image/gif'];
 
+		$dataShopModel = new ShopModel;
+    	$shop = $dataShopModel->find($id);
+		var_dump($shop);
+
+		if(!empty($_POST)){
+
+			$post = array_map('trim', array_map('strip_tags', $_POST));
+
+			/*if((strlen($post['name']) < 1) && (strlen($post['name']) > 20)) {
+				$errors[] = 'Le nom doit comporter  entre 1 et 20 caractères ';
+			}*/
+
+			if(strlen($post['commercialName'])< 1 || strlen($post['commercialName']) > 20) {
+				$errors[] = 'Le nom commercial doit comporter  entre 1 et 20 caractères ';
+			}
+			
+			if(strlen($post['raisonSocial']) < 2 || strlen($post['raisonSocial']) > 30) {
+				$errors[] = 'La raison sociale doit comporter  entre 2 et 30 caractères ';
+			}
+
+			if(!is_numeric($post['immatriculation']) || strlen($post['immatriculation']) != 14) {
+				$errors[] = 'Le numéro SIRET doit comporter  uniquement 14 chiffres !';
+			}
+
+			if(!is_numeric(substr($post['apeCode'],0,3)) && !is_string(substr($post['apeCode'],4,4))) {
+				$errors[] = 'Le code APE comporte 4 chiffres + 1 lettre !';
+			}
+
+			if(strlen($post['nameGerant']) < 1 || strlen($post['nameGerant']) > 40) {
+				$errors[] = 'Le nom du gérant doit comporter  entre 1 et 40 caractères ';
+			}
+
+			if(!filter_var($post['email'], FILTER_VALIDATE_EMAIL)) {
+				$errors[] = "Votre email n'est pas conforme";
+			}
+			
+			
+			if(strlen($post['adress']) < 2 || strlen($post['adress']) > 30) {
+				$errors[] = 'Votre adresse ne semble pas complète. Elle doit comporter  entre 2 et 30 caractères ';
+			}
+			if(strlen($post['city']) < 2 || strlen($post['city']) > 30) {
+				$errors[] = 'La ville doit comporter  entre 2 et 30 caractères ';
+			}
+			if(!is_numeric($post['zipcode']) || strlen($post['zipcode']) < 5) {
+				$errors[] = 'Le code postal doit comporter uniquement 5 chiffres ! ';
+			}
+
+// AJOUT LOGO
+		//$uploadDir = '/'.$post['commercialName'].'/uploads/'; // Répertoire d'upload
+
+			if(isset($_FILES['logo']) && $_FILES['logo']['error'] === 0){
+
+				$finfo = new \finfo();
+				$mimeType = $finfo->file($_FILES['logo']['tmp_name'], FILEINFO_MIME_TYPE);
+
+				$extension = pathinfo($_FILES['logo']['name'], PATHINFO_EXTENSION);
+
+					if(in_array($mimeType, $mimeTypeAvailable)){
+
+			if($_FILES['logo']['size'] <= $maxSize){
+
+				$uploadPath = $_SERVER['DOCUMENT_ROOT'].$_SERVER['W_BASE'].'/assets/'; // Répertoire d'upload
+
+				$uploadDir = 'upload/'.$post['commercialName'].'/'; 
+
+				//var_dump($uploadDir); //var_dump($_FILES);
+
+				if(!is_dir($uploadPath.$uploadDir)){
+					mkdir($uploadPath.$uploadDir, 0755);
+				}
+
+				$newPictureName = uniqid('logo_').'.'.$extension;
+				//$newPictureName = 'logo.'.$extension;
+
+				//if(!move_uploaded_file($_FILES['cover']['tmp_name'], $uploadDir.$newPictureName)){
+					if(!move_uploaded_file($_FILES['logo']['tmp_name'], $uploadPath.$uploadDir.$newPictureName)){
+					$errors[] = 'Erreur lors de l\'upload du logo';
+				}
+			}
+			else {
+				$errors[] = 'La taille du fichier excède 2 Mo';
+			}
+
+				}
+				else {
+					$errors[] = 'Le fichier n\'est pas une image valide';
+				}
+			}
+			else {
+				$errors[] = 'Aucune photo sélectionnée';
+			}
+// FIN GESTION LOGO
+
+			if(count($errors) === 0){
+				
+				$result = '<p style="color:green">Les informations saisies sont correctes !</p>';
+			// ecriture dans la base
+				$post['logo'] = $uploadDir.$newPictureName;
+				
+				$dataShopModel->update($post);
+			}
+			else { // Si j'ai des erreurs
+
+				$result = '<p style="color:red">'.implode('<br>', $errors).'</p>';
+				$shop = $post;
+			}
+
+			echo $result;
 		
-		$info = ['shop' => $viewShop];
-		$this->redirectToRoute('shop_viewShop', $);
+//*************************************************************************
+		}
+
+		$params = ['shop'=> $shop];
+		var_dump($params);
+		$this->show('shop/updateShop', $params); //view 
+
 	}
+	
+	//	$info = ['shop' => $viewShop];
+	//	$this->redirectToRoute('shop_viewShop', $);
+
+
+
 
 	public function deleteShop($id)
 	{
--- app/Views/shop/updateShopold.php
+++ app/Views/shop/updateShopold.php
@@ -31,24 +31,24 @@
 
     <label class="col-md-3 control-label" for="commercialName">Nom commercial</label>  
     <div class="col-md-3">
-        <input id="commercialName" name="commercialName" type="text" value="<?=(isset($shop['commercialName'])) ? $shop['commercialName'] : '';?>" placeholder="Nom de la boutique" class="form-control input-md">
+        <input id="commercialName" name="commercialName" type="text" value="<?=$viewshop['commercialName'];?>" placeholder="Nom de la boutique" class="form-control input-md">
     </div>
 
     <label <!-- class="col-md-3 control-label --> " for="name">Raison Sociale</label>  
     <div class="col-md-3">
-        <input id="raisonSocial" name="raisonSocial" type="text" value="<?=(isset($shop['raisonSocial'])) ? $shop['raisonSocial'] : '';?>" placeholder="Dénomination Administrative" class="form-control input-md">
+        <input id="raisonSocial" name="raisonSocial" type="text" value="<?=$viewshop['raisonSocial'];?>" placeholder="Dénomination Administrative" class="form-control input-md">
     </div>
 </div>
 
 <div class="form-group">
     <label class="col-md-3 control-label" for="immatriculation">Immatriculation au RC</label>  
     <div class="col-md-3">
-        <input id="immatriculation" name="immatriculation" type="text" value="<?=(isset($shop['immatriculation'])) ? $shop['immatriculation'] : '';?>" placeholder="N° SIRET, ex. 404 833 048 00022" class="form-control input-md">
+        <input id="immatriculation" name="immatriculation" type="text" value="<?=$viewshop['immatriculation'];?>" placeholder="N° SIRET, ex. 404 833 048 00022" class="form-control input-md">
     </div>
 
     <label class="col-md-3 control-label" for="apeCode">Code APE</label>  
     <div class="col-md-3">
-        <input id="apeCode" name="apeCode" type="text" value="<?=(isset($shop['apeCode'])) ? $shop['apeCode'] : '';?>" placeholder="47.51Z, 47.63Z... " class="form-control input-md">
+        <input id="apeCode" name="apeCode" type="text" value="<?=$viewshop['apeCode'];?>" placeholder="47.51Z, 47.63Z... " class="form-control input-md">
     </div>
 </div>
 
@@ -59,12 +59,12 @@
 <div class="form-group">
   <label class="col-md-3 control-label" for="nameGerant">Gérant</label>  
     <div class="col-md-3">
-        <input id="nameGerant" name="nameGerant" type="text" value="<?=(isset($shop['nameGerant'])) ? $shop['nameGerant'] : '';?>" placeholder="Prénom et Nom du gérant" class="form-control input-md">
+        <input id="nameGerant" name="nameGerant" type="text" value="<?=$viewshop['nameGerant'];?>" placeholder="Prénom et Nom du gérant" class="form-control input-md">
     </div>
 
 <label class="col-md-3 control-label" for="email">Adresse Email</label>  
     <div class="col-md-3">
-        <input id="email" name="email" type="email" value="<?=(isset($shop['email'])) ? $shop['email'] : '';?>" placeholder="ex. donaldtrompe@tentivement.bar" class="form-control input-md">
+        <input id="email" name="email" type="email" value="<?=$viewshop['email'];?>" placeholder="ex. donaldtrompe@tentivement.bar" class="form-control input-md">
     </div>
 </div>
 </fieldset>
@@ -74,25 +74,25 @@
 <div class="form-group">
 <label class="col-md-3 control-label" for="adress">Adresse</label>  
     <div class="col-md-4">
-        <input id="adress" name="adress" type="text" value="<?=(isset($shop['adress'])) ? $shop['adress'] : '';?>" placeholder="N°, Rue, Impasse..." class="form-control input-md">
+        <input id="adress" name="adress" type="text" value="<?=$viewshop['adress'];?>" placeholder="N°, Rue, Impasse..." class="form-control input-md">
     </div>
 
 
 <label class="col-md-3 control-label" for="zipcode">Code Postal</label>  
     <div class="col-md-2">
-        <input id="zipcode" name="zipcode" type="text" value="<?=(isset($shop['zipcode'])) ? $shop['zipcode'] : '';?>" placeholder="97200" class="form-control input-md">
+        <input id="zipcode" name="zipcode" type="text" value="<?=$viewshop['zipcode'];?>" placeholder="97200" class="form-control input-md">
     </div>
 
 <label class="col-md-3 control-label" for="city">Ville</label>  
     <div class="col-md-3">
-        <input id="city" name="city" type="text" value="<?=(isset($shop['city'])) ? $shop['city'] : '';?>" placeholder="Ville" class="form-control input-md">
+        <input id="city" name="city" type="text" value="<?=$viewshop['city'];?>" placeholder="Ville" class="form-control input-md">
     </div>
 </div>
 </fieldest>
 
 <label class="col-md-4 control-label" for="logo">Logo de la Boutique</label>  
     <div class="col-md-4">
-        <input id="logo" name="logo" type="file" value="<?=(isset($shop['raisonSocial'])) ? $shop['raisonSocial'] : '';?>" placeholder="Images à afficher sur le market" class="form-control input-md">
+        <input id="logo" name="logo" type="file" value="<?=$viewshop['logo'];?>" placeholder="Images à afficher sur le market" class="form-control input-md">
     </div>
 <!-- <label class="col-md-4 control-label" for="cover">Images Société</label>  
     <div class="col-md-4">
